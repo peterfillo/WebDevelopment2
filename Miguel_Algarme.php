3cf61439fc1636f8e7cd73aabbc59cbc33e3356a
MiguelAlgarme <21102947@usc.edu.ph>
2022-08-24 16:08:56 +0800

    MAPA

---
+++ Miguel_Algarme.php
@@ -0,0 +1,92 @@
+<?php
+echo "prank em johnscript ";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <style>
+
+    body 
+    {
+            font-family: 'Open Sans', sans-serif;
+    }
+  
+    *
+    {
+    padding: 0;
+    margin: 0;
+    text-decoration: none;
+    list-style: none;
+    box-sizing: border-box;
+    }
+
+
+    nav
+    {
+    position: relative;
+    top: 10px;
+    height: 50px;
+    width: 100%;
+    background-color: rgba(76, 175, 80, 0.3);
+    }
+    a 
+    {
+    color: black;
+    }
+    nav ul{
+    float: right;
+    margin-right: 20px;
+    }
+
+    nav ul li{
+    display: inline-block;
+    line-height: 80px;
+    margin: 0 5px;
+    }
+
+    nav ul li a{
+    color: black;
+    font-size: 17px;
+    padding: 7px 13px;
+    border-radius: 3px;
+    text-transform: uppercase;
+    }
+
+ 
+    </style>
+    <title>Too much coffee, I might die.</title>
+</head>
+
+
+
+<body>
+    <div class="panel">
+    <nav>
+    <ul >
+            <li><a href="#">&emsp;<b>Leads to nowhere yet</b></a></li>
+            <li><a href="#">&emsp;<b>Leads to nowhere yet too</b></a></li>
+            <li><a href="#">&emsp;<b>Bruh</b></a></li>
+            <li><a href="#">&emsp;<b>:I</b></a></li>
+        </ul> 
+    </nav>
+    </div>
+    <h1>:III</h1>
+
+        <center>
+        <p><b>Chrome wont allow autoplay, click the video to watch gecko jingle jangle in the mojave</b></p>
+        <iframe width="500" height="350" autoplay
+    src="https://www.youtube.com/embed/IVRlYIjKy0A">
+    </iframe> 
+        </center>
+
+
+</body>
+</html>
+
+
