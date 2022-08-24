5c3485f576ab0602a349a62dbedc7e5cfaf95aab
Joshua Chiu <21100768@usc.edu.ph>
2022-08-24 16:16:32 +0800

    Added file from joshua

---
+++ Joshua_Chiu.php
@@ -0,0 +1,51 @@
+<?php include "header.php";?>
+<link href="explore.css" type="text/css" rel="stylesheet">
+<div class = "cont">
+<div class="topnav2">
+                <h2><center>Explore Page</center></h2>
+            </div>
+<div class="mainbox">
+            <div style="margin: 20px">
+                <div class="gallery2">
+                  <div class="desc">
+                  <h2 style="text-align: center;">Lifestyle Quizzes</h2>
+                    <ul style><br><br>
+                        <a href = "https://quizizz.com/admin/quiz/5bba47e1371e69001afe1024/importance-of-exercise" class = "desc2">Exercise Meal Diets 1</li></a>
+                        <br><br><br>
+                        <a href = "https://www.vocabulary.com/lists/166798" class = "desc2">Exercise 2 Vocabulary</li></a>
+                        <br><br><br>
+                        <a href = "https://www.proprofs.com/quiz-school/topic/exercise" class = "desc2">Exercise 3 Training</li></a>
+                        <br><br><br>
+                       </ul>
+                      </div>
+                  </div>
+                <div class="gallery">
+                    <a target="_blank" href="https://www.sahealth.sa.gov.au/wps/wcm/connect/public+content/sa+health+internet/healthy+living/healthy+mind/healthy+mind">
+                      <img src="photoJ/medicine.jpg" width="600" height="400">
+                    </a>
+                    <div class="desc">On Mental Health</div>
+                    <div class="desc"><a href="personal.html"><input type="submit" value="Click to learn" class="bton"></a></div>
+                  </div>
+                  
+                  <div class="gallery">
+                    <a target="_blank" href="https://www.betterhealth.vic.gov.au/health/healthyliving/running-and-jogging-health-benefits">
+                      <img src="photoJ/jog.png" width="600" height="400">
+                    </a>
+                    <div class="desc">Health Advices</div>
+                    <div class="desc"><a href="personal.html">
+                      <input type="submit" value="Click to learn" class="bton"></a></div>
+                  </div>
+                  
+                  <div class="gallery">
+                    <a target="_blank" href="https://www.mentalhealth.gov/basics/what-is-mental-health">
+                      <img src="photoJ/mind.jpg" width="600" height="400">
+                    </a>
+                    <div class="desc">Alternative Exercises</div>
+                    <div class="desc"><a href="personal.html"><input type="submit" value="Click to learn" class="bton"></a></div>
+                  </div>
+                </div>
+          </div>
+      </div>
+      </div>
+</div>
+<?php include "footer.php";?>
