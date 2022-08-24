c404b46bcae36e36d741644e892fb51d502a1b25
Diaphos <21100768@usc.edu.ph>
2022-08-24 19:40:13 +0800

    test file pc

---
+++ diet.php
@@ -0,0 +1,283 @@
+<?php include "header.php";?>
+<link href="diet.css" type="text/css" rel="stylesheet">
+<div class ="cont">
+  <div class="topnav2">
+    <h2 style="text-align: center;">Diet</h2>
+  </div>
+  <div class="row">
+    <div class="column left">
+      <h2>Articles</h2>
+      <div class="wrapper">
+        <div class="card">
+          <a href="https://www.healthline.com/nutrition/best-diet-plans" target="_blank">
+            <span class="link-spanner"></span>
+          </a>
+          <div class="card-banner">
+            <img class="banner-img" src='Diet/diet.jpg' alt='ryan'>
+          </div>
+          <div class="card-body">
+            <h2 class="blog-title">The 9 Best Diet Plans for Your Overall Health</h2>
+            <p class="blog-description">Diets aren’t just for weight loss. While changing your diet can be one of the
+              best ways to lose...</p>
+            <div class="card-profile">
+              <img class="profile-img" src='Diet/ryan.jpg' alt=''>
+              <div class="card-profile-info">
+                <h3 class="profile-name">Ryan Raman</h3>
+                <p class="profile-date">March 27, 2022</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <a href="https://www.healthline.com/nutrition/best-diet-plans" target="_blank">
+            <span class="link-spanner"></span>
+          </a>
+          <div class="card-banner">
+            <img class="banner-img" src='Diet/diet.jpg' alt='ryan'>
+          </div>
+          <div class="card-body">
+            <h2 class="blog-title">The 9 Best Diet Plans for Your Overall Health</h2>
+            <p class="blog-description">Diets aren’t just for weight loss. While changing your diet can be one of the
+              best ways to lose...</p>
+            <div class="card-profile">
+              <img class="profile-img" src='Diet/ryan.jpg' alt=''>
+              <div class="card-profile-info">
+                <h3 class="profile-name">Ryan Raman</h3>
+                <p class="profile-date">March 27, 2022</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <a href="https://www.healthline.com/nutrition/best-diet-plans" target="_blank">
+            <span class="link-spanner"></span>
+          </a>
+          <div class="card-banner">
+            <img class="banner-img" src='Diet/diet.jpg' alt='ryan'>
+          </div>
+          <div class="card-body">
+            <h2 class="blog-title">The 9 Best Diet Plans for Your Overall Health</h2>
+            <p class="blog-description">Diets aren’t just for weight loss. While changing your diet can be one of the
+              best ways to lose...</p>
+            <div class="card-profile">
+              <img class="profile-img" src='Diet/ryan.jpg' alt=''>
+              <div class="card-profile-info">
+                <h3 class="profile-name">Ryan Raman</h3>
+                <p class="profile-date">March 27, 2022</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column right">
+      <h2>Plans</h2>
+      <div class="tab">
+        <button class="tablinks" onclick="openDiet(event, 'Breakfast')" id="defaultOpen">Breakfast</button>
+        <button class="tablinks" onclick="openDiet(event, 'Lunch')">Lunch</button>
+        <button class="tablinks" onclick="openDiet(event, 'Dinner')">Dinner</button>
+        <button class="tablinks" onclick="openDiet(event, 'Snacks')">Snacks</button>
+        <button class="tablinks" onclick="openDiet(event, 'Plant-based')">Plant-based</button>
+      </div>
+      <div id="Breakfast" class="tabcontent">
+        <center>
+          <h3>Breakfast Diet Plans</h3>
+          <p><i>Kickstart your day with some healthy breakfast diets!</i></p>
+        </center>
+        <hr>
+        <ul>
+          <li>
+            <img src="Diet/spinach.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Spinach & Cheese Breakfast Skillet</a></h3>
+            <p><b>Spinach & Cheese Breakfast Skillet:</b> Eating a big protein-rich breakfast may help lower your levels
+              of the hunger-signaling hormone, ghrelin, and reduce snack cravings later in the day. In this healthy
+              hash-and-egg recipe, a whopping 34 grams of protein per serving will keep you feeling satisfied through
+              until lunch.</p>
+          </li>
+          <li>
+            <img src="Diet/nut.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Nut & Berry Parfait</a></h3>
+            <p><b>Nut & Berry Parfait:</b> Swapping in Greek yogurt for regular delivers twice the protein and makes you
+              morning meal super satisfying. In this quick high-protein parfait recipe, fiber-rich berries, sliced
+              almonds, and a drizzle of honey top Greek yogurt for a healthy, filling and delicious breakfast with 30
+              grams of protein per serving.</p>
+          </li>
+          <li>
+            <img src="Diet/strawberry.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Strawberry-Banana Protein Smoothie</a></h3>
+            <p><b>Strawberry-Banana Protein Smoothie:</b> Pairing protein and fiber-rich foods together, makes for a
+              meal with double the staying power. In this healthy smoothie recipe, 19 grams of protein from Greek yogurt
+              and nut butter, and 10 grams of fiber from fruit and ground flaxseed combine to create a energy-packed
+              breakfast that will help keep you full all morning long.</p>
+          </li>
+          <li>
+            <img src="Diet/raspberry.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Raspberry Overnight Muesli</a></h3>
+            <p><b>Raspberry Overnight Muesli:</b> Regular yogurt, oats and fruit combine and are topped with sliced
+              almonds in this healthy overnight muesli recipe. You'll get 17 grams of protein to help you feel full
+              during the morning hours. To cut back on added sugars, make this recipe with plain yogurt instead of
+              vanilla.</p>
+          </li>
+        </ul>
+      </div>
+
+      <div id="Lunch" class="tabcontent">
+        <center>
+          <h3>Lunch Diet Plans</h3>
+          <p><i>Halfway through the day? Check out these recipes for that final stretch.</i></p>
+        </center>
+        <hr>
+        <ul>
+          <li>
+            <img src="Diet/cheese.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Tomato-&-Avocado Cheese Sandwich</a></h3>
+            <p><b>Tomato-&-Avocado Cheese Sandwich:</b> Parmesan cheese is such a bold flavor, you only need 1/4 cup to
+              add a big punch to this vegetarian toaster-oven sandwich. Get a serving of fruit too, when you enjoy the
+              sandwich with a fresh pear.</p>
+          </li>
+          <li>
+            <img src="Diet/open-face.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Open-Face Edd Salad Sandwich</a></h3>
+            <p><b>Open-Face Edd Salad Sandwich:</b> This egg salad recipe is spiked with dill and lemon zest. Crisp
+              pancetta adds texture and flavor. If you don't want to go with the pancetta, try a few capers sprinkled on
+              top instead.</p>
+          </li>
+          <li>
+            <img src="Diet/chickpea.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Chickpea Salad</a></h3>
+            <p><b>Chickpea Salad:</b> Feta cheese and chickpeas lend a Mediterranean flair to this satisfying side
+              salad. The Creamy Dill Ranch is great with it, but would also be good with a tangy vinaigrette.</p>
+          </li>
+          <li>
+            <img src="Diet/veggie.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Veggie & Hummus Sandwich</a></h3>
+            <p><b>Veggie & Hummus Sandwich:</b> This mile-high vegetable and hummus sandwich makes the perfect
+              heart-healthy vegetarian lunch to go. Mix it up with different flavors of hummus and different types of
+              vegetables depending on your mood.</p>
+          </li>
+        </ul>
+      </div>
+
+      <div id="Dinner" class="tabcontent">
+        <center>
+          <h3>Dinner Diet Plans</h3>
+          <p><i>Treat yourself to a healthy and scrumptuous meal at the end of the day with these recipes!</i></p>
+        </center>
+        <hr>
+        <ul>
+          <li>
+            <img src="Diet/zucchini.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Zucchini Noodles with Avocado Pesto & Shrimp</a>
+            </h3>
+            <p><b>Zucchini Noodles with Avocado Pesto & Shrimp:</b> Cut some carbs and use spiralized zucchini in place
+              of noodles in this zesty pesto pasta dish recipe. Top with Cajun-seasoned shrimp to complete this quick
+              and easy dinner.</p>
+          </li>
+          <li>
+            <img src="Diet/pesto.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Pesto Ravioli with Spinach & Tomatoes</a></h3>
+            <p><b>Pesto Ravioli with Spinach & Tomatoes:</b> Looking for a new way to turn a bag of frozen cheese
+              ravioli into a satisfying and healthy dinner? This easy pasta recipe calls for just five ingredients but
+              is loaded with fresh flavors. By using grape tomatoes, prewashed spinach and prepared pesto, we eliminate
+              all prep work, making this 20-minute Caprese-inspired ravioli the ideal weeknight meal.</p>
+          </li>
+          <li>
+            <img src="Diet/roasted.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Roasted Pistachio-Crusted Salmon with Broccoli</a>
+            </h3>
+            <p><b>Roasted Pistachio-Crusted Salmon with Broccoli:</b> This easy one-pan roasted salmon with broccoli is
+              quick enough for weeknight dinners but elegant enough for company. The lemony pistachio crust would also
+              be lovely on other types of fish or on chicken breasts.</p>
+          </li>
+          <li>
+            <img src="Diet/skillet.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Skillet Ravioli Lasagna</a></h3>
+            <p><b>Skillet Ravioli Lasagna:</b> This easy inside-out ravioli lasagna is the ultimate weeknight comfort
+              food--no layering or mixing bowls required. Feel free to swap in ground turkey for the beef. Look for
+              fresh mozzarella balls (also called "pearls") in the specialty cheese section of your grocery store.</p>
+          </li>
+        </ul>
+      </div>
+      <div id="Snacks" class="tabcontent">
+        <center>
+          <h3>Snack Diet Plans</h3>
+          <p><i>Feeling low on energy? Try these snack recipes to keep you going!</i></p>
+        </center>
+        <hr>
+        <ul>
+          <li>
+            <img src="Diet/low-carb.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Low-Carb Blueberry Muffins</a></h3>
+            <p><b>Low-Carb Blueberry Muffins:</b> Just because you're watching carbs doesn't mean you can't have the
+              classic breakfast pastry. In place of all-purpose flour, nutty almond and coconut flours make these
+              muffins fluffy while slashing carbs. A bit of brown sugar blunts the tartness of fresh blueberries. Make
+              these ahead for a grab-and-go breakfast for the week.</p>
+          </li>
+          <li>
+            <img src="Diet/chocolate.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Chocolate-Peanut Butter Energy Bars</a></h3>
+            <p><b>Chocolate-Peanut Butter Energy Bars:</b> Dates provide all the sweetness you need for these no-bake
+              energy bars. Each square serves up a hearty dose of protein--thanks to peanut butter and peanuts--as well
+              as fiber from rolled oats. Kids will love the chewy bites with crunchy nuts. </p>
+          </li>
+          <li>
+            <img src="Diet/peanut.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Peanut Butter-Chocolate Apple Dippers</a></h3>
+            <p><b>Peanut Butter-Chocolate Apple Dippers:</b> Caramel apples are a delicious treat but can contain up to
+              60 grams of carbs. Satisfy your sweet craving with these lower-carb apple slices, dipped in peanut butter
+              and sprinkled with chocolate pieces.</p>
+          </li>
+          <li>
+            <img src="Diet/blackberry.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Blackberry & Cream Cheese Cracker</a></h3>
+            <p><b>Blackberry & Cream Cheese Cracker:</b> Fresh blackberries and light cream cheese make a pretty, sweet
+              topping for a crispbread snack, especially with a little mint sprinkled on top. Besides being beautiful
+              and delicious, blackberries are packed with antioxidants and vitamins.</p>
+          </li>
+        </ul>
+      </div>
+      <div id="Plant-based" class="tabcontent">
+        <center>
+          <h3>Plant-based Diet Plans</h3>
+          <p><i>Develop your love for fruits and vegetables with these plant-based recipes!</i></p>
+        </center>
+        <hr>
+        <ul>
+          <li>
+            <img src="Diet/beefless.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Beefless Vegan Tacos</a></h3>
+            <p><b>Beefless Vegan Tacos:</b> Take taco night in a new direction with these healthy vegan tacos. We've
+              swapped crumbled tofu for the ground beef, without sacrificing any of the savory seasonings you expect in
+              a taco. You can also use the filling in burritos, bowls, taco salads and to top nachos.</p>
+          </li>
+          <li>
+            <img src="Diet/quinoa.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Quinoa-Black Bean Salad</a></h3>
+            <p><b>Quinoa-Black Bean Salad:</b> Enjoy this quinoa and black bean salad as a delicious and quick
+              vegetarian main dish or as a side for grilled chicken or steak. And don't forget the leftovers! They make
+              an easy lunch on the go.</p>
+          </li>
+          <li>
+            <img src="Diet/thai.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Thai-Style Chopped Salad with Sriracha Tofu</a>
+            </h3>
+            <p><b>Thai-Style Chopped Salad with Sriracha Tofu:</b> Prep four days' worth of high-protein vegan lunches
+              using just four easy ingredients from your local specialty grocery store, including a veggie-heavy salad
+              mix as a base. Because this salad mix is hearty, you can dress these bowls up to 24 hours before serving
+              to allow the flavors in this healthy chopped salad to marry. If you can't find a hearty mix, go with
+              broccoli slaw or shredded Brussels sprouts.</p>
+          </li>
+          <li>
+            <img src="Diet/greek.jpg" />
+            <h3><a href="https://www.eatingwell.com/" target="_blank">Greek Salad with Edamame</a></h3>
+            <p><b>Greek Salad with Edamame:</b> Edamame adds protein to the classic Greek salad: romaine, tomatoes,
+              cucumber, feta and olives. Serve with toasted pita brushed with olive oil and sprinkled with dried oregano
+              or za'atar.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+</div>
+</div>
+<script src="diet.js"></script>
+<?php include "footer.php";?>
