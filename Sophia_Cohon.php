db8a68070a44a86927aad560a6075a968a1b77e2
Sophia Cohon <19103447@usc.edu.ph>
2022-08-26 15:21:52 +0800

    My First commit

---
+++ Sophia_Cohon.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            padding: 0px;
+        }
+        h1{
+            background-color: aliceblue;
+        }
+        #magic{
+            padding: 10px;
+            background-color: #A7C7E7;
+            border-radius: 5px;
+            border-style: hidden;
+        }
+    </style>
+    <script>
+        function myFunction() {
+            document.getElementById("first").innerHTML = "This button can change text because of JavaScript."
+        }
+    </script>
+</head>
+<body>
+    <h1 id="first">Hello World! Welcome to my <i>simple website.</i> </h1>
+    <button type="button" onclick="myFunction()" id="magic">Click me</button>
+    <br>
+    <br>
+    <?php 
+    echo "This is a code in PHP. Hello World!";
+    ?>
+</body>
+</html>
